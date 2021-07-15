bd30bf9956ce4f25f4eb299636fd9bf6626c6e21
moirahartigan <moirahartigan@gmail.com>
2021-07-15 10:59:27 +0000

    Add hello function

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-# python code goes here
+# Python Text Based Game - The Scarecrows Wedding
+# Story taken and adapted from a book of the same name By Julia Donaldson
+# By Moira Hartigan
+
+def hello():
+    name = str(input("Hello! Tell me your name: "))
+    print("Hi " + str(name) + ". Welcome to The Scarecrows Wedding")
+    return
+hello()
+
+
