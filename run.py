9686e887d2b2f754e189f56b21c10f599a2d8e01
moirahartigan <moirahartigan@gmail.com>
2021-07-19 14:45:01 +0000

    Change typewriter name to print_text

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ print()
 
 
 # typewriter style animation code adpted from https://www.youtube.com/watch?v=2h8e0tXHfk0
-def typewriter(intro): 
+def print_text(intro): 
     """
     This function allows the text to be displayed letter by letter using sys import rater than a
     whole block of text - A time delay added will apply at the end of each 
@@ -52,12 +52,13 @@ def typewriter(intro):
         else:
             time.sleep(0.03) # 0.5 sec delay for the end of each sentance
 
-typewriter(intro)    
+print_text(intro)    
 welcome_msg()
 
 
 def intro():
     """
+
     Intro function begins the game and welcomes the user
     by requesting their name in order to play
     """
@@ -71,9 +72,9 @@ def intro():
             continue
         else:
             break
-    typewriter(f"Welcome to Diddly Squat Farm {name}, \n") 
-    typewriter("Thank you for joining us\n")
-    typewriter("for the Scarecrows Wedding.\n")
+    print_text(f"Welcome to Diddly Squat Farm {name}, \n") 
+    print_text("Thank you for joining us\n")
+    print_text("for the Scarecrows Wedding.\n")
     print()
     start_game()
 
@@ -82,7 +83,7 @@ def start_game():
     while True:
         startGame = input("Would you like to start the game? \n (yes/no): \n")
         if startGame == 'no' or startGame == 'No':
-            typewriter("Maybe next time. Goodbye")
+            print_text("Maybe next time. Goodbye")
             print()
             game_over()
         
@@ -91,7 +92,7 @@ def start_game():
             chapter_one()
 
         else:
-            typewriter("Hmmmm .... I dont understand that. Please type yes or no.\n")
+            print_text("Hmmmm .... I dont understand that. Please type yes or no.\n")
             print()
             
                     
@@ -101,20 +102,20 @@ def chapter_one():
     Function to move the player into the first chapter of the game 
     once they have agreed to play
     """     
-    typewriter("Ok Great ! \n")
-    typewriter("Here is the story so far ...... \n")
-    typewriter("Harry loved Betty and Betty loved Harry, \n")
-    typewriter('So Harry said, “Betty, my beauty, lets marry!\n')
-    typewriter("Lets have a wedding, the best wedding yet,\n")
-    typewriter('A wedding no one will ever forget.”\n')
+    print_text("Ok Great ! \n")
+    print_text("Here is the story so far ...... \n")
+    print_text("Harry loved Betty and Betty loved Harry, \n")
+    print_text('So Harry said, “Betty, my beauty, lets marry!\n')
+    print_text("Lets have a wedding, the best wedding yet,\n")
+    print_text('A wedding no one will ever forget.”\n')
     print()
-    typewriter("Betty agreed, so they hugged \n")
-    typewriter("and they kissed. \n")
-    typewriter('Then Betty said, “Harry, dear, \n')
-    typewriter('Lets make a list.” \n')
+    print_text("Betty agreed, so they hugged \n")
+    print_text("and they kissed. \n")
+    print_text('Then Betty said, “Harry, dear, \n')
+    print_text('Lets make a list.” \n')
     print()
-    typewriter('Harry gave Betty his arm \n')
-    typewriter("And set off on a hunt round the farm. \n")
+    print_text('Harry gave Betty his arm \n')
+    print_text("And set off on a hunt round the farm. \n")
     print()
     print()
     while True:
@@ -125,12 +126,12 @@ def chapter_one():
             chapter_two()
 
         elif first_response == 'no' or first_response == 'No':
-            typewriter("Too bad, it would have been the best wedding yet. Goodbye.")
+            print_text("Too bad, it would have been the best wedding yet. Goodbye.")
             print()
             game_over()
         
         else:
-            typewriter("Hmmmm .... I dont understand that. Please type yes or no.\n")
+            print_text("Hmmmm .... I dont understand that. Please type yes or no.\n")
             print()
        
 def chapter_two():
@@ -140,18 +141,18 @@ def chapter_two():
     otherwise the game ends. This level allows the player a choice for their 
     response.
     """
-    typewriter("Pink Flowers were the only thing left on the list \n")
-    typewriter('Harry said “Betty, dear, I can find those. \n')
-    typewriter('Why don’t I pick some while you have a doze?” \n')
+    print_text("Pink Flowers were the only thing left on the list \n")
+    print_text('Harry said “Betty, dear, I can find those. \n')
+    print_text('Why don’t I pick some while you have a doze?” \n')
     print()
-    typewriter("I can find you a field of pink flowerzzzzz! \n")
-    typewriter("Follow me! ..... \n")
-    typewriter("Buzzed a big stripy bee. \n")
+    print_text("I can find you a field of pink flowerzzzzz! \n")
+    print_text("Follow me! ..... \n")
+    print_text("Buzzed a big stripy bee. \n")
     print()
-    typewriter("What should Harry do? \n ")
-    typewriter("1.) Follow the bee \n")
-    typewriter(" 2.) Go to the farm shop to buy some flowers instead \n")
-    typewriter(" 3.) Call the florist and have them deliver the flowers \n")
+    print_text("What should Harry do? \n ")
+    print_text("1.) Follow the bee \n")
+    print_text(" 2.) Go to the farm shop to buy some flowers instead \n")
+    print_text(" 3.) Call the florist and have them deliver the flowers \n")
     print()
     while True:
         second_response = input("Choose option (1, 2 or 3): \n ")
@@ -161,20 +162,20 @@ def chapter_two():
             chapter_three()
         elif second_response == '2':
             print()
-            typewriter(" oh no the Farm Shop is sold out\n")
-            typewriter(" Betty is not pleased\n")
-            typewriter(" The Wedding is off !")
+            print_text(" oh no the Farm Shop is sold out\n")
+            print_text(" Betty is not pleased\n")
+            print_text(" The Wedding is off !")
             print()
             game_over()
         elif second_response == '3':
             print()
-            typewriter("You are a Scarecrrow !!! \n")
-            typewriter("You have no phone to make the call and no money to pay for flowers.\n ")
+            print_text("You are a Scarecrrow !!! \n")
+            print_text("You have no phone to make the call and no money to pay for flowers.\n ")
             print()
             game_over()
 
         else:
-            typewriter("Hmmmm .... I dont understand that. Please type yes or no.\n")
+            print_text("Hmmmm .... I dont understand that. Please type yes or no.\n")
             print()        
 
 
@@ -183,10 +184,10 @@ def chapter_three():
     Function to allow player to move to chapter three
     and looks for the player to give a yes/ no response
     """   
-    typewriter("So the bee led the way, and they travelled for hours\n")
-    typewriter("Till they come to a field full of pretty pink flowers.\n")
-    typewriter('Harry is now thinking "I wont pick them yet.\n')
-    typewriter('I will need to find water, to keep their stalks wet.” \n')
+    print_text("So the bee led the way, and they travelled for hours\n")
+    print_text("Till they come to a field full of pretty pink flowers.\n")
+    print_text('Harry is now thinking "I wont pick them yet.\n')
+    print_text('I will need to find water, to keep their stalks wet.” \n')
     print()
     while True:
         third_response = input("Do you think Harry needs to find water?: \n (yes/no)\n")
@@ -195,14 +196,14 @@ def chapter_three():
             chapter_four()
 
         elif third_response == 'no' or third_response == 'No':        
-            typewriter("Harry returns with the flowers..... \n")
-            typewriter("but they have died without water!\n")
-            typewriter("oh dear Betty is not pleased and the wedding is off.\n")
+            print_text("Harry returns with the flowers..... \n")
+            print_text("but they have died without water!\n")
+            print_text("oh dear Betty is not pleased and the wedding is off.\n")
             print()
             game_over()
 
         else:
-            typewriter("Hmmmm .... I dont understand that. Please type yes or no.\n")
+            print_text("Hmmmm .... I dont understand that. Please type yes or no.\n")
             print()
 
 
@@ -213,15 +214,15 @@ def chapter_four():
     here the player uses yes/no again to choose an option 
     """
     print()
-    typewriter('"Just follow me",\n')
-    typewriter("croaked a lumpy old toad.\n")
-    typewriter('There is a lovely wet pool at the top of this road."\n')
+    print_text('"Just follow me",\n')
+    print_text("croaked a lumpy old toad.\n")
+    print_text('There is a lovely wet pool at the top of this road."\n')
     print()
-    typewriter("They climbed up the road.\n")
-    typewriter("It was terribly steep.\n")
+    print_text("They climbed up the road.\n")
+    print_text("It was terribly steep.\n")
     print()
-    typewriter('"Im tired,” said the toad,\n')
-    typewriter('"Will we stop for a sleep?"\n')
+    print_text('"Im tired,” said the toad,\n')
+    print_text('"Will we stop for a sleep?"\n')
     print()
     while True:
         forth_response = input("Do you think Harry should stop for a sleep?: \n (yes/no)\n")
@@ -230,13 +231,13 @@ def chapter_four():
             chapter_five()
 
         elif forth_response == 'yes' or forth_response == 'Yes':        
-            typewriter("Oh dear, Harry overslept .....\n")
-            typewriter("he missed the wedding!\n")
+            print_text("Oh dear, Harry overslept .....\n")
+            print_text("he missed the wedding!\n")
             print()
             game_over()
 
         else:
-            typewriter("Hmmmm .... I dont understand that. Please type yes or no.\n")
+            print_text("Hmmmm .... I dont understand that. Please type yes or no.\n")
             print()
 
 
@@ -245,15 +246,15 @@ def chapter_five():
     The story continues to chapter 5 and a yes/ no response is requested 
     here again
     """
-    typewriter("Wise choice ! \n")
+    print_text("Wise choice ! \n")
     print()
-    typewriter("Just over the hill they came to a pool.\n")
-    typewriter('"This water," said Harry, "is beautifully cool,\n')
-    typewriter("But now I need something to carry it in –\n")
-    typewriter('A jug or a vase or a cup or a tin."\n')
+    print_text("Just over the hill they came to a pool.\n")
+    print_text('"This water," said Harry, "is beautifully cool,\n')
+    print_text("But now I need something to carry it in –\n")
+    print_text('A jug or a vase or a cup or a tin."\n')
     print()
-    typewriter('"I think I can help," said a small squirly snail.\n')
-    typewriter('"I can show you the way to a very fine pail."\n')
+    print_text('"I think I can help," said a small squirly snail.\n')
+    print_text('"I can show you the way to a very fine pail."\n')
     while True:
         fifth_response = input("Should Harry follow the snail ? \n (yes/no)\n")
         if fifth_response == 'no' or fifth_response == 'No':
@@ -261,18 +262,18 @@ def chapter_five():
             chapter_six()
 
         elif fifth_response == 'yes' or fifth_response == 'Yes':        
-            typewriter("So the snail and the scarecrow \n")
-            typewriter("Set off on their way,\n")
+            print_text("So the snail and the scarecrow \n")
+            print_text("Set off on their way,\n")
             print()
-            typewriter("But the snail was so slow …\n")
-            typewriter("… it took more than a day.\n")
+            print_text("But the snail was so slow …\n")
+            print_text("… it took more than a day.\n")
             print()
-            typewriter("Harry has missed the wedding\n")
+            print_text("Harry has missed the wedding\n")
             print()
             game_over()
         
         else:
-            typewriter("Hmmmm .... I dont understand that. Please type yes or no.\n")
+            print_text("Hmmmm .... I dont understand that. Please type yes or no.\n")
             print()
 
 
@@ -282,22 +283,22 @@ def chapter_six():
     the user can read the ending, they are thanked for their help
     """
     print()
-    typewriter("Good Advice\n")
-    typewriter("Harry fills up his hat\n")
-    typewriter("And runs back for the pink flowers to put in his hat\n")
+    print_text("Good Advice\n")
+    print_text("Harry fills up his hat\n")
+    print_text("And runs back for the pink flowers to put in his hat\n")
     print()
-    typewriter("Suddenly\n")
+    print_text("Suddenly\n")
     print()
-    typewriter("Who should appear on the farm\n")
-    typewriter("But Harry O’Hay,\n")
-    typewriter("With a hat of pink flowers in his arm.\n")
+    print_text("Who should appear on the farm\n")
+    print_text("But Harry O’Hay,\n")
+    print_text("With a hat of pink flowers in his arm.\n")
     print()
-    typewriter("So Betty O’Barley and Harry O’Hay\n")
-    typewriter("Wed one another the very next day\n")
+    print_text("So Betty O’Barley and Harry O’Hay\n")
+    print_text("Wed one another the very next day\n")
     print()
-    typewriter(f"Thanks for your help {name}, \n")
-    typewriter("You helped make the best wedding ever, the best wedding yet,\n")
-    typewriter("The wedding that no one will ever forget.\n")
+    print_text(f"Thanks for your help {name}, \n")
+    print_text("You helped make the best wedding ever, the best wedding yet,\n")
+    print_text("The wedding that no one will ever forget.\n")
     print()
     happy_ending()
 
